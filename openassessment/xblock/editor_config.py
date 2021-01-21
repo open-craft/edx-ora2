34a1e25b5f58635e73ae3c086b06a9d7c00d1df3
Shimul Chowdhury <shimul@opencraft.com>
2021-01-21 21:38:56 +0600

    Keep only textarea related code

--- openassessment/xblock/editor_config.py
+++ openassessment/xblock/editor_config.py
@@ -10,10 +10,6 @@ default_editors = {
         'display_name': 'Simple Text Editor',
         'js': ['/xblock/resource/openassessment/static/js/openassessment-editor-textarea.js'],
     },
-    'tinymce': {
-        'display_name': 'WYSIWYG Editor',
-        'js': ['/xblock/resource/openassessment/static/js/openassessment-editor-tinymce.js'],
-    },
 }
 
 AVAILABLE_EDITORS = {}
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -14,8 +14,7 @@ Object.assign(config, {
   entry: {
     "openassessment-lms": path.resolve(process.cwd(), 'openassessment/xblock/static/js/src/lms_index.js'),
     "openassessment-studio": path.resolve(process.cwd(), 'openassessment/xblock/static/js/src/studio_index.js'),
-    "openassessment-editor-textarea": path.resolve(process.cwd(), 'openassessment/xblock/static/js/src/lms/editors/oa_editor_textarea.js'),
-    "openassessment-editor-tinymce": path.resolve(process.cwd(), 'openassessment/xblock/static/js/src/lms/editors/oa_editor_tinymce.js'),
+    "openassessment-editor-textarea": path.resolve(process.cwd(), 'openassessment/xblock/static/js/src/lms/editors/oa_editor_textarea.js')
   },
   output: {
     path: path.resolve(process.cwd(), 'openassessment/xblock/static/js'),
