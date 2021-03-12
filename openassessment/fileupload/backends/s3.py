851c23f4cc5c13d3d6b3cd1a2bf1e4bde8daa38e
Raul Gallegos <raul@opencraft.com>
2021-03-11 20:13:55 -0500

    updates s3 communication

--- openassessment/fileupload/backends/s3.py
+++ openassessment/fileupload/backends/s3.py
@@ -5,8 +5,9 @@ import logging
 
 from django.conf import settings
 
-import boto
+import botocore
 from botocore.config import Config as BotoConfig
+import boto3
 
 from ..exceptions import FileUploadInternalError
 from .base import BaseBackend
@@ -21,40 +22,43 @@ class Backend(BaseBackend):
         bucket_name, key_name = self._retrieve_parameters(key)
         try:
             conn = _connect_to_s3()
-            upload_url = conn.generate_url(
-                expires_in=self.UPLOAD_URL_TIMEOUT,
-                method='PUT',
-                bucket=bucket_name,
-                key=key_name,
-                headers={'Content-Length': '5242880', 'Content-Type': content_type}
+            return conn.generate_presigned_url(
+                "put_object",
+                Params={
+                    "Bucket": bucket_name,
+                    "Key": key_name,
+                    "ContentType": content_type,
+                },
+                ExpiresIn=self.UPLOAD_URL_TIMEOUT,
             )
-            return upload_url
         except Exception as ex:
             logger.exception(
                 u"An internal exception occurred while generating an upload URL."
             )
-            raise FileUploadInternalError(ex)
+            raise FileUploadInternalError(ex) from ex
 
     def get_download_url(self, key):
         bucket_name, key_name = self._retrieve_parameters(key)
         try:
             conn = _connect_to_s3()
-            bucket = conn.get_bucket(bucket_name)
-            s3_key = bucket.get_key(key_name)
-            return s3_key.generate_url(expires_in=self.DOWNLOAD_URL_TIMEOUT) if s3_key else ""
+            if not object_exists(conn, bucket_name, key_name):
+                return ""
+            return conn.generate_presigned_url(
+                "get_object",
+                Params={"Bucket": bucket_name, "Key": key_name},
+                ExpiresIn=self.DOWNLOAD_URL_TIMEOUT,
+            )
         except Exception as ex:
             logger.exception(
                 u"An internal exception occurred while generating a download URL."
             )
-            raise FileUploadInternalError(ex)
+            raise FileUploadInternalError(ex) from ex
 
     def remove_file(self, key):
         bucket_name, key_name = self._retrieve_parameters(key)
         conn = _connect_to_s3()
-        bucket = conn.get_bucket(bucket_name)
-        s3_key = bucket.get_key(key_name)
-        if s3_key:
-            bucket.delete_key(s3_key)
+        if object_exists(conn, bucket_name, key_name):
+            conn.delete_object(Bucket=bucket_name, Key=key_name)
             return True
         return False
 
@@ -63,17 +67,32 @@ def _connect_to_s3():
     """Connect to s3
 
     Creates a connection to s3 for file URLs.
-
     """
     # Try to get the AWS credentials from settings if they are available
-    # If not, these will default to `None`, and boto will try to use
+    # If not, these will default to `None`, and boto3 will try to use
     # environment vars or configuration files instead.
-    aws_access_key_id = getattr(settings, 'AWS_ACCESS_KEY_ID', None)
-    aws_secret_access_key = getattr(settings, 'AWS_SECRET_ACCESS_KEY', None)
+    aws_access_key_id = getattr(settings, "AWS_ACCESS_KEY_ID", None)
+    aws_secret_access_key = getattr(settings, "AWS_SECRET_ACCESS_KEY", None)
+    endpoint_url = getattr(settings, "AWS_S3_ENDPOINT_URL", None)
 
     config = BotoConfig(signature_version='v4')
-    return boto.connect_s3(
+    return boto3.client(
+        "s3",
         aws_access_key_id=aws_access_key_id,
         aws_secret_access_key=aws_secret_access_key,
+        endpoint_url=endpoint_url,
         config=config,
     )
+
+
+def object_exists(conn, bucket_name, key_name):
+    """
+    Check if a key exists in the given S3 bucket.
+    """
+    try:
+        conn.head_object(Bucket=bucket_name, Key=key_name)
+    except botocore.exceptions.ClientError as e:
+        if e.response["Error"]["Code"] == "404":
+            return False
+        raise e
+    return True
